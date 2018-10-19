a8876aef5bc71abfa38f0fcbf0637beb9b94810e
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-19 18:11:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   name_hash.each do |k, v|
     if lowest_value == nil || v < lowest_value
-      lowest_value
-      lowest_key
+      lowest_value = v
+      lowest_key = k
     end
   end
   lowest_key
